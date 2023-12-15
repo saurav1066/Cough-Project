b00ce0f59b427b720d30d742ee7eca15b8f96a1c
saurav1066 <sauravadhikari35@gmail.com>
2023-12-15 09:24:50 +0100

    Added cough bout Plot

--- Cough-app/app.R
+++ Cough-app/app.R
@@ -98,7 +98,8 @@ ui <- fluidPage(
     tabPanel("Plots",
              mainPanel(
                h3("Other Plots"),
-               plotlyOutput(outputId = "cough_plotly")
+               plotlyOutput(outputId = "cough_plotly"),
+               plotlyOutput(outputId = "bout_cough")
              )
     ),
     
@@ -323,6 +324,37 @@ server <- function(input, output, session) {
                                                          scale_color_gradient(low = "blue", high = "red") +
                                                          labs(x = "Hours", y = "Minutes", color = "Seconds") +
                                                          theme_minimal()
+          )) 
+          
+          #Plot with bouts
+          data<- format(time_cough, "%H:%M:%S")
+          # Convert to datetime
+          data <- hms(data)
+          
+          
+          #converting to dataframe
+          
+          data<- data.frame(time = data, index = c(1:length(data)))
+          
+          # Convert time to POSIXct
+          data$time <- as.POSIXct(strptime(data$time, format = "%HH %MM %SS"), origin = "1970-01-01")
+          
+          # Calculate time differences and group
+          data <- data %>%
+            mutate(diff = c(0, as.numeric(difftime(time[-1], time[-length(time)], units = "secs")))) %>%
+            mutate(group = ifelse(diff <= 6 & diff > 1, TRUE, FALSE)) %>%
+            mutate(group = ifelse(lead(group, default = group[n()]), TRUE, group))
+          
+          
+          
+          data
+          
+          # Plot
+          output$bout_cough <- renderPlotly(ggplotly(ggplot(data, aes(x = time, y = index, color = as.factor(group))) +
+                                                       geom_point() +
+                                                       scale_x_datetime(labels = scales::time_format("%H:%M:%S")) +
+                                                       labs(x = "Time (Hours:Minutes:Seconds)", y = "Index", color = "Group") +
+                                                       theme_minimal()
           )) 
           
           # Counting the occurrences of "cough" for each hour of the day
