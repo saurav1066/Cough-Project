35fa3af28f791d542b59e833700bc4daa4038f22
saurav1066 <sauravadhikari35@gmail.com>
2023-12-19 09:12:52 +0100

    Fixed y axis for better visualization

--- Cough-app/app.R
+++ Cough-app/app.R
@@ -287,6 +287,7 @@ server <- function(input, output, session) {
             scale_color_manual(values = c("TRUE" = "red", "FALSE" = "blue")) +
             labs(x = "Hours", y = "Frequency of Cough") +
             ggtitle(paste("Cough Plot for" ,unique(grouped_df[[i]]$VISIT) ))+
+            scale_y_continuous(limits = c(0,300))+
             theme_minimal()
           
           plot_list[[i]] <- plot
@@ -526,6 +527,7 @@ server <- function(input, output, session) {
             geom_point() +
             scale_color_manual(values = c("TRUE" = "red", "FALSE" = "blue")) +
             labs(x = "Hours", y = "Frequency of Cough") +
+            scale_y_continuous(limits = c(0,300))+
             theme_minimal()
         } 
         
