17e52d866fcf5f58576295fbf7eee995bd799e27
saurav1066 <sauravadhikari35@gmail.com>
2023-12-20 10:03:00 +0100

    Basic layout designed

--- patient/app.R
+++ patient/app.R
@@ -8,42 +8,28 @@
 #
 
 library(shiny)
+library(readxl)
 
-# Define UI for application that draws a histogram
+#reading required files
+ds = read_excel("ds.xlsx")
+pro = read_excel("pro.xlsx")
+
+#Converting to dataframe
+subject_list <- data.frame(ds)
+input_file <- data.frame(pro)
+
+#UI
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  mainPanel(
+    h3("Plot")
+    plotOutput(outputId = "plot")
+    
+  )
+  
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+server <- function(input, output, session) {
+  
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
