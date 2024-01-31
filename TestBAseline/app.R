7c18405d061e362203cb25dfb9aeae3189a08d9c
saurav1066 <sauravadhikari35@gmail.com>
2024-01-31 11:06:58 +0100

    Added plots for baseline sex

--- TestBAseline/app.R
+++ TestBAseline/app.R
@@ -63,7 +63,7 @@ hourly_counts_table <- function(df, sleep, wake){
                                   (sleep_time <= wake_time
                                    & (hourly_counts$Var1 >= sleep_time
                                       & hourly_counts$Var1 <= wake_time))
-                                , TRUE, FALSE) hashtag#Simple rearranging according to the start of recording time
+                                , TRUE, FALSE)  #Simple rearranging according to the start of recording time
   hourly_counts <- hourly_counts[order((hourly_counts$Var1 - start_time)
                                        %% nrow(hourly_counts)),]
   
@@ -101,6 +101,7 @@ sidebarLayout(
       selectInput(inputId = "Visit",
                   label = "Select Visit Number",
                   choices = result$visit_id)
+    )
     ),
     
     #Condition Comparision
@@ -115,6 +116,7 @@ sidebarLayout(
       selectInput(inputId = "Visit",
                   label = "Select Visit Number",
                   choices = result$visit_id)
+    )
     ),
     
     #Condition Baseline
@@ -177,88 +179,137 @@ sidebarLayout(
 server <- function(input, output, session) {
   observe({ 
     #Condition BAseline
-    if(input$Condition == 'Baseline'){ 
-      
-      #Selecting required data from the input
-      selected_data <- reactive({
-        result$cough_data[result$cough_data$VISIT == input$Visit,]
-      })
-      
-      
-      new_df <- selected_data()
-      
-      
-      # Generate UI elements dynamically
-      id_list <- lapply(subject_id, as.character)
-      
-      output$baseline_plot <- renderUI({
-        lapply(id_list, function(id) {
-          plotOutput(id)
+    if(input$Condition == 'Baseline'){
+      if(input$baseline_condition == 'Visit'){ #Selecting required data from the input
+        selected_data <- reactive({
+          result$cough_data[result$cough_data$VISIT == input$baseline_Visit,]
         })
-      })
-      
-      
-      # Render plots for each ID
-      lapply(id_list, function(subject) {
         
-        output[[subject]] <- renderPlot({
-          df <- new_df[new_df$USUBJID == as.numeric(subject),] 
-          
-          #Getting sleep, wake and start time of recording values
-          sleep_time <- as.integer(df$hour[df$FAOBJ == "Sleep"])
-          wake_time <- as.integer(df$hour[df$FAOBJ == "Wake"])
-          
-          
-          start_time<- 0 # needs initialization because some subjects do not have a record
-          
-          start_time <- as.integer(df$hour[df$FAOBJ == "Actual Recording Start Time"])
-          
-          
-          # Counting the occurrences of "cough" for each hour of the day
-          hourly_counts <- table(df$hour[df$FAOBJ == "Cough"])
-          hourly_counts <- data.frame(hourly_counts)
-          
-          #Converting the hour data to integer
-          hourly_counts$Var1 <- as.integer(as.character(hourly_counts$Var1))
-          print(nrow(hourly_counts))
-          
-          if (nrow(hourly_counts) == 0){
-            ggplot() +
-              ggtitle(paste("Cough Plot for" ,subject) )+
-              labs(x = "Hour", y = "Count", title = "No data found for the given Visit ID and USUBJID")
-          }
-          else { 
-           
-            #Calling Helper Function
-            hourly_counts<- hourly_counts_table(df, sleep_time, wake_time)
-            
-            # Plot with sleep indicators for each visit
-            ggplot(hourly_counts,
-                   aes(x = factor(Var1, levels = unique(Var1)),
-                       y = Freq, color = sleep, group=1)) +
-              geom_line() +
-              geom_point() +
-              ggtitle(paste("Cough Plot for" ,subject) )+
-              scale_color_manual(values = c("TRUE" = "red", "FALSE" = "blue")) +
-              labs(x = "Hours", y = "Frequency of Cough") +
-              scale_y_continuous(limits = c(0, 290))+
-              theme_minimal()
-            
-            
-            
-          }
+        
+        new_df <- selected_data()
+        
+        
+        # Generate UI elements dynamically
+        id_list <- lapply(subject_id, as.character)
+        
+        output$baseline_plot <- renderUI({
+          lapply(id_list, function(id) {
+            plotOutput(id)
+          })
         })
-      })
+        
+        
+        # Render plots for each ID
+        lapply(id_list, function(subject) {
+          
+          output[[subject]] <- renderPlot({
+            df <- new_df[new_df$USUBJID == as.numeric(subject),]  #Getting sleep, wake and start time of recording values
+            sleep_time <- as.integer(df$hour[df$FAOBJ == "Sleep"])
+            wake_time <- as.integer(df$hour[df$FAOBJ == "Wake"])
+            
+            
+            start_time<- 0 # needs initialization because some subjects do not have a record
+            
+            start_time <- as.integer(df$hour[df$FAOBJ == "Actual Recording Start Time"])
+            
+            
+            # Counting the occurrences of "cough" for each hour of the day
+            hourly_counts <- table(df$hour[df$FAOBJ == "Cough"])
+            hourly_counts <- data.frame(hourly_counts)  #Converting the hour data to integer
+            hourly_counts$Var1 <- as.integer(as.character(hourly_counts$Var1))
+            
+            if (nrow(hourly_counts) == 0){
+              ggplot() +
+                ggtitle(paste("Cough Plot for" ,subject) )+
+                labs(x = "Hour", y = "Count", title = paste0("No data found for ", subject , ' in ', input$baseline_Visit ))
+            }
+            else {  #Calling helper function
+              hourly_counts <- hourly_counts_table(df, sleep_time , wake_time)
+              
+              # Plot with sleep indicators for each visit
+              ggplot(hourly_counts,
+                     aes(x = factor(Var1, levels = unique(Var1)),
+                         y = Freq, color = sleep, group=1)) +
+                geom_line() +
+                geom_point() +
+                ggtitle(paste("Cough Plot for" ,subject) )+
+                scale_color_manual(values = c("TRUE" = "red", "FALSE" = "blue")) +
+                labs(x = "Hours", y = "Frequency of Cough") +
+                scale_y_continuous(limits = c(0, 290))+
+                theme_minimal()
+              
+              
+              
+            }
+          })
+        })
+      }
+      else{  #Selecting Filtered sex data
+        selected_extension <- reactive({
+          result$extension_data[result$extension_data$SEX == input$baseline_Sex,]
+          
+        })
+        
+        extension <- selected_extension()
+        
+        # Generate UI elements dynamically
+        id_list <- lapply(extension$SUBJID, as.character)
+        
+        output$baseline_plot <- renderUI({
+          lapply(id_list, function(id) {
+            plotOutput(id)
+          })
+        })
+        
+        
+        
+        # Render plots for each ID
+        lapply(id_list, function(subject) {
+          
+          
+          output[[subject]] <- renderPlot({
+            df <- result$cough_data[result$cough_data$USUBJID == as.numeric(subject),]
+            
+            if(nrow(df) != 0) {  #grouping the dataframe based on visits
+              grouped_df <- split(df, df$VISIT,df$USUBJID)  #Initilizing for later use to store plots and dataframes
+              cough_counts <- data.frame()  #getting hourly counts of each group of visits
+              for (i in 1:length(grouped_df)) {  #Getting the hour where the subject slept
+                sleep_time <- as.integer(grouped_df[[i]]$hour[grouped_df[[i]]$FAOBJ == "Sleep"])  #Getting the hour where the subject woke up
+                wake_time <- as.integer(grouped_df[[i]]$hour[grouped_df[[i]]$FAOBJ == "Wake"])  #Getting the hour where the recording started
+                start_time <- as.integer(grouped_df[[i]]$hour[grouped_df[[i]]$FAOBJ == "Actual Recording Start Time"])  #Calling helper function
+                hourly_count <- hourly_counts_table(grouped_df[[i]], sleep_time,wake_time)  #Adding Visit Column in the hourly count table
+                hourly_count$Visit <- grouped_df[[i]]$VISIT[1]  #Storing all cough counts in a separate dataframe
+                cough_counts <- rbind(cough_counts, hourly_count)
+              }  #plotting all the visits in same graph
+              ggplot(cough_counts,
+                     aes(x = Var1, y = Freq, group = Visit, color = Visit)) +
+                geom_line() +
+                geom_point()+
+                ggtitle(paste0("Comparision of all visits for subject ", subject)) +
+                labs(x = "Hours", y = "Frequency of Cough")+
+                theme_minimal()
+            }
+            else{
+              ggplot() +
+                ggtitle(paste("Cough Plot for" ,subject) )+
+                labs(x = "Hour", y = "Count", title = paste0("No data found for ", subject ))
+            }
+            
+            
+          })
+        })
+        
+        
+      }
       
     }
-    
     #Condition Single Insight
     else if (input$Condition == 'Single Insight'){
       # Selecting required data as per input
       selected_data <- reactive({
         result$cough_data[result$cough_data$VISIT == input$Visit &
                             result$cough_data$USUBJID == input$SubjectID,]
-      }) hashtag#Selecting required extension data
+      })  #Selecting required extension data
       
       selected_extension <- reactive({
         result$extension_data[result$extension_data$SUBJID == input$SubjectID,]
@@ -271,15 +322,15 @@ server <- function(input, output, session) {
       output$cough_plot <- renderPlot({
         
         
-        if (nrow(new_df) > 0) { hashtag#Getting sleep, wake and start time of recording values
+        if (nrow(new_df) > 0) {  #Getting sleep, wake and start time of recording values
           sleep_time <- as.integer(new_df$hour[new_df$FAOBJ == "Sleep"])
           wake_time <- as.integer(new_df$hour[new_df$FAOBJ == "Wake"])
           
           
           start_time<- 0 # needs initialization because some subjects do not have a record
           
-          start_time <- as.integer(new_df$hour[new_df$FAOBJ == "Actual Recording Start Time"]) hashtag#Calling helper Function
-          hourly_counts <- hourly_counts_table(new_df, sleep_time,wake_time) hashtag#Characteristics Table
+          start_time <- as.integer(new_df$hour[new_df$FAOBJ == "Actual Recording Start Time"])  #Calling helper Function
+          hourly_counts <- hourly_counts_table(new_df, sleep_time,wake_time)  #Characteristics Table
           output$characteristics_table <- render_gt({
             gt_table <- gt(extension)
             
